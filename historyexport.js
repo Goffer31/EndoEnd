1077ee54af64c0d50d58b1a5734d2acaf2a526c3
Mateusz <harlon132@gmail.com>
2020-11-02 20:37:59 +0100

    Checkpoint using rest request

--- historyexport.js
+++ historyexport.js
@@ -20,10 +20,22 @@ let workoutslinks = require('./workoutslinks.json');
     });
 
     let client = await browser.newPage();
-    await client.goto(workoutslinks[0]);
-    await client.evaluate(() => {
+    //console.log('https://www.endomondo.com/rest/v1/users/' + workoutslinks[0].split('/users/')[1]+'/export?format=TCX');
+    try {
+        await client.goto('https://www.endomondo.com/rest/v1/users/' + workoutslinks[0].split('/users/')[1]+'/export?format=TCX');
+    } catch(error){
+        console.log('Error: ' + error);
+    }
+    
+    /*await client.evaluate(() => {
         let dropdown = document.getElementsByClassName('dropdown-menu menu workoutHeadline-editMenu eoWorkoutEditMenu ng-isolate-scope');
         dropdown[0].children[11].click();
     });
+
+    await client.evaluate(() => {
+        let dropdown = document.getElementsByClassName('col-xs-12');
+        dropdown[0].children[0].click();
+    });*/
+
     //browser.disconnect();
 })()
